289c02664ee4c9f1686876520973c74e9d9037dd
unknown0218 <investorola01@gmail.com>
2025-06-30 14:39:27 -0700

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,20 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
+// Middleware to parse form and JSON data
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Serve static files from public directory
 app.use(express.static(path.join(__dirname, "public")));
+
+// Handle POST request for form submission
+app.post("/login", (req, res) => {
+  const { username, password } = req.body;
+  console.log("Received login data:", { username, password });
+  // For now, send a simple response (extend with authentication logic later)
+  res.send("Login attempt received! Username: " + username);
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
